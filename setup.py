fd5425cbd225914364e7635fa9ea30b3cc87aa00
pranayreddy99 <112796997+pranayreddy99@users.noreply.github.com>
2025-04-26 01:04:22 -0500

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import find_packages, setup
+
+setup(
+    name = 'Generative AI Project for Medical Health Record',
+    version= '0.0.0',
+    author= 'Pranay Reddy',
+    author_email= 'guradipranay99@gmail.com',
+    packages= find_packages(),
+    install_requires = []
+
+)
